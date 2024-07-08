8ba72c5dcf1c8570f53a7aa0896ff31b218cc650
d3Yv3r <deyverson@astrotech.solutions>
2024-07-08 17:51:44 -0300

    feat: finished home page creation

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,56 +1,66 @@
 import { CarCard } from "@/components/CarCard";
-import { useState } from "react";
-// import { Modal } from "@/components/Modal";
+import { useEffect, useState } from "react";
 import { Drawer } from "@/components/Drawer";
-
-const carData = [
-  {
-    image: "images/carsOne.jpg",
-    price: "R$ 50.000,00",
-    brand: "Toyota",
-    model: "Corolla",
-    year: 2020,
-    views: 1234,
-  },
-  {
-    image: "images/carsOne.jpg",
-    price: "R$ 50.000,00",
-    brand: "Toyota",
-    model: "Corolla",
-    year: 2020,
-    views: 1234,
-  },
-  {
-    image: "images/carsOne.jpg",
-    price: "R$ 50.000,00",
-    brand: "Toyota",
-    model: "Corolla",
-    year: 2020,
-    views: 1234,
-  },
-  {
-    image: "images/carsOne.jpg",
-    price: "R$ 50.000,00",
-    brand: "Toyota",
-    model: "Corolla",
-    year: 2020,
-    views: 1234,
-  },
-];
+import { Columns2, Rows3 } from "lucide-react";
+import { twMerge } from "tailwind-merge";
 
 export const Home = () => {
   const [open, setOpen] = useState(false);
+  const [data, setData] = useState([]);
+  const [carData, setCarData] = useState();
+  const [isGrid, setIsGrid] = useState(false);
 
-  const handleClickOpen = () => {
+  const handleRequest = () =>
+    fetch("http://192.168.18.32:3000/cars")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+
+  const handleChangeStyle = () => {
+    setIsGrid(!isGrid);
+  };
+
+  const handleClickOpen = async (id) => {
+    let carId = data.find((item) => item.id === id);
+    carId = { ...carId, views: carId.views + 1 };
+    await fetch(`http://192.168.18.32:3000/cars/${id}`, {
+      method: "put",
+      body: JSON.stringify(carId),
+    });
+    handleRequest();
+    setCarData(carId);
     setOpen(true);
   };
+
+  useEffect(() => {
+    handleRequest();
+  }, []);
   return (
     <div className="flex flex-col gap-4">
-      {carData.map((item, index) => (
-        <CarCard key={index} {...item} handleClickOpen={handleClickOpen} />
-      ))}
-      {/* <Modal open={open} setOpen={setOpen} /> */}
-      <Drawer open={open} setOpen={setOpen} />
+      <button
+        onClick={() => handleChangeStyle()}
+        className="w-fit ml-auto col-span-2"
+      >
+        {!isGrid ? (
+          <div className="flex justify-end flex-row gap-2 bg-primary-100 p-2 rounded text-white">
+            <Rows3 />
+            <span>Lista</span>
+          </div>
+        ) : (
+          <div className="flex justify-end flex-row gap-2 bg-primary-100 p-2 rounded text-white">
+            <Columns2 />
+            <span>Grade</span>
+          </div>
+        )}
+      </button>
+
+      <div
+        className={twMerge("flex flex-col gap-4", isGrid && "grid grid-cols-2")}
+      >
+        {data?.map((item, index) => (
+          <CarCard key={index} {...item} handleClickOpen={handleClickOpen} />
+        ))}
+      </div>
+      <Drawer open={open} setOpen={setOpen} carData={carData} />
     </div>
   );
 };
