bb6b979f678d48a003d5e99b9af2750f6c948a47
d3Yv3r <deyverson@astrotech.solutions>
2024-07-08 22:17:20 -0300

    chore: general adjustments

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,7 +1,29 @@
+import { CirclePlus } from "lucide-react";
+import { useEffect, useState } from "react";
+import { CarTable } from "./components/CarTable";
+
 export const Admin = () => {
+  const [tableData, setTableData] = useState([]);
+
+  const handleRequest = () =>
+    fetch("http://192.168.0.8:3000/cars")
+      .then((response) => response.json())
+      .then((json) => setTableData(json));
+
+  useEffect(() => {
+    handleRequest();
+  }, []);
+
   return (
-    <>
-      <div>Admin</div>
-    </>
+    <div className="flex flex-col gap-4 h-screen">
+      <button onClick={() => {}} className="w-fit ml-auto col-span-2">
+        <div className="flex justify-end flex-row gap-2 bg-primary-100 p-2 rounded text-white">
+          <CirclePlus />
+          <span>Adicionar</span>
+        </div>
+      </button>
+
+      <CarTable tableData={tableData} />
+    </div>
   );
 };
